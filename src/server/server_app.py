9af3b6ea2a080d8e5d2fe3ddd6d5bcdfc419787f
cmh02 <cmh0201@auburn.edu>
2026-04-19 00:29:19 -0500

    Stubbing server

---
+++ src/server/server_app.py
@@ -0,0 +1,23 @@
+'''
+	# HSSFLDON - Server Application
+
+	This module will provide the server application for HSSFLDON.
+	
+	The server is responsible for:
+	- Managing unlabeled dataset
+	- Distributing unknown datapoints to clients for labeling
+	- Aggregating client updates in global model
+
+
+
+'''
+
+
+class HSSFLDON_ServerApplication:
+	"""
+	The main server application for HSSFLDON.
+	"""
+	def __init__(self):
+		pass
+
+	
---
+++ src/server/server_main.py
@@ -0,0 +1,16 @@
+'''
+	# HSSFLDON - Server Application
+
+	This will be the main entrypoint for the server in HSSFLDON.
+	
+	The server is responsible for:
+	- Managing unlabeled dataset
+	- Distributing unknown datapoints to clients for labeling
+	- Aggregating client updates in global model
+
+
+
+'''
+
+if __name__ == "__main__":
+	pass
