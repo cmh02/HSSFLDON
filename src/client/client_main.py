ba0819b8b3f46772713522bcdd758fa967ee0d4b
cmh02 <cmh0201@auburn.edu>
2026-04-19 00:29:16 -0500

    Stubbing client

---
+++ src/client/client_main.py
@@ -0,0 +1,11 @@
+'''
+	# HSSFLDON - Client Application
+
+	This module will provide the client application for HSSFLDON.
+	
+	The client is responsible for:
+	- Managing localized labeled dataset
+	- Training local model
+	- Receiving active labeling requests
+	- Uploading model updates to server
+'''
