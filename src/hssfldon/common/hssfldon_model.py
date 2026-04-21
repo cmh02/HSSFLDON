4f02afda603f0cec7e42cbbfceb63fbf211ba680
cmh02 <cmh0201@auburn.edu>
2026-04-20 19:56:34 -0500

    Added common model manager for working with SLM

---
+++ src/hssfldon/common/hssfldon_model.py
@@ -0,0 +1,77 @@
+'''
+	# HSSFLDON - Model
+
+	This common module will provide the model for use by the server and clients in HSSFLDON.
+'''
+
+### Library Imports
+import os
+from dotenv import load_dotenv
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import LoraConfig, get_peft_model, PeftModel
+import torch
+
+# Project Imports
+from hssfldon.common.hssfldon_logger import HSSFLDON_Logger
+
+class HSSFLDON_ModelManager:
+    """
+    The main model class for HSSFLDON.
+    """
+    def __init__(self, modelId: str = "meta-llama/Llama-3.2-1B"):
+
+        # Get logger
+        self.logger: HSSFLDON_Logger = HSSFLDON_Logger(name=f"ModelManager")
+
+        # Copy in params
+        self.modelId: str = modelId
+
+        # Load env file and needed values
+        load_dotenv(dotenv_path='../.env')
+        self.loraRank: int = int(os.getenv("HSSFLDON_LORA_RANK", 8))
+        self.loraAlpha: int = int(os.getenv("HSSFLDON_LORA_ALPHA", 32))
+
+        # Setup base model
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.base_model = AutoModelForCausalLM.from_pretrained(
+            self.modelId, 
+            torch_dtype=torch.float16,
+            device_map=self.device
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(self.modelId)
+        self.lora_config = LoraConfig(r=self.loraRank, lora_alpha=self.loraAlpha, target_modules=["q_proj", "v_proj"])
+        self.logger.info(f"Initialized base model `{self.modelId}` on device `{self.device}` with LoRA config: `rank={self.loraRank}`, `alpha={self.loraAlpha}`!")
+
+    def getFreshModel(self) -> PeftModel:
+        """
+        Get a fresh instance of the model.
+
+        Returns:
+            An instance of the model.
+        """
+        return get_peft_model(self.base_model, self.lora_config)
+
+    def loadAdapterFromFile(self, filePath: str) -> PeftModel:
+        """
+        Load a model adapter from a file.
+
+        Args:
+            filePath: The path to the adapter file.
+        
+        Returns:
+            An instance of the model with the adapter loaded.
+        """
+        if not os.path.exists(filePath):
+            raise FileNotFoundError(f"Adapter file not found at path: {filePath}")
+        
+        return PeftModel.from_pretrained(model=self.base_model, adapter_name=filePath, is_trainable=True)
+
+    def saveAdapterToFile(self, model: PeftModel, filePath: str):
+        """
+        Save a model adapter to a file.
+
+        Args:
+            model: The model instance with the adapter to save.
+            filePath: The path to save the adapter file to.
+        """
+        model.save_pretrained(save_directory=filePath)
