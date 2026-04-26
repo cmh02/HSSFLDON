0a96b0211efa2126e1731721bb2baa3c7efbf201
cmh02 <cmh0201@auburn.edu>
2026-04-26 13:02:33 -0500

    Initial mock pipeline completed for active learning

--- src/hssfldon/server/server_app.py
+++ src/hssfldon/server/server_app.py
@@ -20,6 +20,8 @@ import torch
 import uvicorn
 import requests
 import threading
+import torch.nn.functional as F
+from sklearn.cluster import KMeans
 from datasets import load_dataset, Dataset
 from fastapi import FastAPI
 from dotenv import load_dotenv
@@ -54,6 +56,25 @@ class HSSFLDON_ServerApplication:
 		self.clientTasks: dict[int, HSSFLDON_ClientTask] = {}
 		self.clientUpdateStatus: dict[int, bool] = {}
 		self.clientHeadPathCache: dict[int, str | None] = {}
+
+		# Intialize index-category mapping
+		self.indexCategoryMapping: dict[int, str] = {}
+		self.indexCategoryMapping[0] = "sentiment"
+		self.indexCategoryMapping[1] = "respect"
+		self.indexCategoryMapping[2] = "insult"
+		self.indexCategoryMapping[3] = "humiliate"
+		self.indexCategoryMapping[4] = "status"
+		self.indexCategoryMapping[5] = "dehumanize"
+		self.indexCategoryMapping[6] = "violence"
+		self.indexCategoryMapping[7] = "genocide"
+		self.indexCategoryMapping[9] = "attack_defend"
+		self.indexCategoryMapping[10] = "hatespeech"
+
+		# Initialize client oracle mapping
+		self.clientOracleMapping: dict[int, list[int]] = {}
+		self.clientOracleMapping[0] = [2, 5, 6]
+		self.clientOracleMapping[1] = [7, 9, 1]
+		self.clientOracleMapping[2] = [0, 3, 10]
 
 		# Setup API
 		self.api_host = os.getenv("HSSFLDON_SERVER_HOST", "127.0.0.1")
@@ -93,6 +114,10 @@ class HSSFLDON_ServerApplication:
 		if self.unlabeledDataset is None:
 			self.logger.error(f"Failed to load dataset from path: {self.dataFilePath}")
 			raise ValueError(f"Dataset loading failed for path: {self.dataFilePath}")
+		self.unlabeledDataloader: torch.utils.data.DataLoader = modelManager.tokenize_and_create_dataloader(
+			texts = self.unlabeledDataset["text"],
+			labels=None
+		)
 
 		# Begin server loop for configured iterations
 		self.learningIterations: int = int(os.getenv("HSSFLDON_LEARNING_ITERATIONS", 10))
@@ -108,51 +133,56 @@ class HSSFLDON_ServerApplication:
 		self.logger.info(f"Starting main server learning loop for {self.learningIterations} iterations!")
 		for iteration in range(self.learningIterations):
 			self.logger.info(f"Starting learning iteration {iteration+1}/{self.learningIterations}!")
-
-			# Passive Learning: Tell clients to perform passive learning
-			self.enterState(HSSFLDON_ServerState.PASSIVE_LEARNING)
-			self.logger.info(f"Performing passive learning for iteration {iteration+1}/{self.learningIterations}!")
-			for clientId in self.clients:
-
-				# Assign passive learning task to client
-				self.clientTasks[clientId] = HSSFLDON_ClientTask.DO_PASSIVE_LEARNING
-				self.clientUpdateStatus[clientId] = False
-
-				# Wait for client update
-				self.logger.debug(f"Waiting for update from client {clientId}!")
-				while not self.clientUpdateStatus[clientId]:
-					time.sleep(1)
-
-				# Tell client to standby until next iteration
-				self.clientTasks[clientId] = HSSFLDON_ClientTask.STANDBY
-				self.logger.debug(f"Received update from client {clientId} and set to standby!")
-
-			# Passive Aggregation: Aggregate client updates into global model for passive learning
-			self.enterState(HSSFLDON_ServerState.AGGREGATING)
-			self.logger.info(f"Aggregating client updates for iteration {iteration+1}/{self.learningIterations}!")
 			modelManager: HSSFLDON_ModelManager = HSSFLDON_ModelManager(customHeadIdentifier=f"global")
-			avgStateDict = self._fedAverageClientUpdates(modelManager=modelManager, clientHeadPaths=self.clientHeadPathCache)
-			if avgStateDict is not None:
-				modelManager.component_head.load_state_dict(avgStateDict)
-				modelManager.saveClassificationHead(head=modelManager.component_head, name=f"classification_head_global.pt")
-				self.logger.info(f"Successfully aggregated client updates for iteration {iteration+1}/{self.learningIterations}!")
-			else:
-				self.logger.warning(f"Failed to aggregate client updates for iteration {iteration+1}/{self.learningIterations}!")
+
+			# # Passive Learning: Tell clients to perform passive learning
+			# self.enterState(HSSFLDON_ServerState.PASSIVE_LEARNING)
+			# self.logger.info(f"Performing passive learning for iteration {iteration+1}/{self.learningIterations}!")
+			# for clientId in self.clients:
+
+			# 	# Assign passive learning task to client
+			# 	self.clientTasks[clientId] = HSSFLDON_ClientTask.DO_PASSIVE_LEARNING
+			# 	self.clientUpdateStatus[clientId] = False
+
+			# 	# Wait for client update
+			# 	self.logger.debug(f"Waiting for update from client {clientId}!")
+			# 	while not self.clientUpdateStatus[clientId]:
+			# 		time.sleep(1)
+
+			# 	# Tell client to standby until next iteration
+			# 	self.clientTasks[clientId] = HSSFLDON_ClientTask.STANDBY
+			# 	self.logger.debug(f"Received update from client {clientId} and set to standby!")
+
+			# # Passive Aggregation: Aggregate client updates into global model for passive learning
+			# self.enterState(HSSFLDON_ServerState.AGGREGATING)
+			# self.logger.info(f"Aggregating client updates for iteration {iteration+1}/{self.learningIterations}!")
+			# avgStateDict = self._fedAverageClientUpdates(modelManager=modelManager, clientHeadPaths=self.clientHeadPathCache)
+			# if avgStateDict is not None:
+			# 	modelManager.component_head.load_state_dict(avgStateDict)
+			# 	modelManager.saveClassificationHead(head=modelManager.component_head, name=f"classification_head_global.pt")
+			# 	self.logger.info(f"Successfully aggregated client updates for iteration {iteration+1}/{self.learningIterations}!")
+			# else:
+			# 	self.logger.warning(f"Failed to aggregate client updates for iteration {iteration+1}/{self.learningIterations}!")
 
 			# Active Preparation: Determine finalist datapoints to send to clients
 			self.enterState(HSSFLDON_ServerState.ACTIVE_LEARNING)
 			self.logger.info(f"Preparing for active learning for iteration {iteration+1}/{self.learningIterations}!")
 			probabilities, _ = modelManager.predict(
-				dataLoader=self.unlabeledDataset,
+				dataLoader=self.unlabeledDataloader,
 				outputType=HSSFLDON_PredictionOutputType.PROBABILITY_PREDICTION
 			)
-			activeDataloader = self.unlabeledDataset.add_column("probabilities", probabilities)
+			activeDataloader = self.unlabeledDataloader.add_column("probabilities", probabilities)
 			finalistDataloader = self._getFinalistDatapointsForActiveLearning(
 				dataLoader=activeDataloader,
 				confidenceThreshold = float(os.getenv("HSSFLDON_ACTIVE_LEARNING_CONFIDENCE", 0.5)),
 				numFinalists=int(os.getenv("HSSFLDON_ACTIVE_LEARNING_NUM_FINALISTS", 10)),
 				numCentroids=int(os.getenv("HSSFLDON_ACTIVE_LEARNING_NUM_CENTROIDS", 5))
 			)
+
+			# For now, print off finalist datapoitns
+			self.logger.info(f"Finalist datapoints for active learning in iteration {iteration+1}/{self.learningIterations}:")
+			for datapoint in finalistDataloader:
+				self.logger.info(f"Datapoint: {datapoint}")
 
 	def launchApi(self) -> bool:
 		"""
@@ -239,7 +269,7 @@ class HSSFLDON_ServerApplication:
 		# Load the averaged state dict into the global model
 		return avgStateDict
 	
-	def _getFinalistDatapointsForActiveLearning(self, dataLoader: DataLoader, confidenceThreshold: float, numFinalists: int, numCentroids: int) -> DataLoader:
+	def _getFinalistDatapointsForActiveLearning(self, modelManager: HSSFLDON_ModelManager, dataLoader: DataLoader, confidenceThreshold: float, numFinalists: int, numCentroids: int) -> DataLoader:
 		"""
 		Get the finalist datapoints to send to clients for active learning based on confidence threshold.
 
@@ -248,3 +278,118 @@ class HSSFLDON_ServerApplication:
 		unconfident datapoints are closest to the centroids of the unlabeled dataset while farthest from
 		the centroids of the labeled dataset.
 		"""
+		self.logger.debug(f"Getting finalist datapoints for active learning with confidence threshold {confidenceThreshold}, num finalists {numFinalists}, and num centroids {numCentroids}!")
+
+		# Get confident vs unconfident datapoints based on confidence threshold
+		confidentDatapoints = []
+		unconfidentDatapoints = []
+		for datapoint in dataLoader:
+			if max(datapoint["probabilities"]) >= confidenceThreshold:
+				confidentDatapoints.append(datapoint)
+			else:
+				unconfidentDatapoints.append(datapoint)
+
+		# If we don't have any unconfident datapoints, just return empty dataloader
+		if not unconfidentDatapoints:
+			self.logger.warning(f"No unconfident datapoints found with confidence threshold {confidenceThreshold}! Returning empty dataloader for active learning finalists!")
+			return DataLoader([])
+
+		# If we have less unconfident datapoints than the number of finalists we want to send, just return all unconfident datapoints
+		if len(unconfidentDatapoints) <= numFinalists:
+			self.logger.warning(f"Number of unconfident datapoints ({len(unconfidentDatapoints)}) is less than or equal to the number of finalists requested ({numFinalists})! Returning all unconfident datapoints for active learning finalists!")
+			return DataLoader(unconfidentDatapoints)
+
+		# Turn unconfident and confident datapoints into dataloaders for processing
+		unconfidentDataloader = DataLoader(unconfidentDatapoints, batch_size=32)
+		confidentDataloader = DataLoader(confidentDatapoints, batch_size=32)
+
+		# Get centroids for unconfident datapoints and confident datapoints
+		unconfidentEmbeddings, unconfidentCentroids = self._getEmbeddingsAndCentroids(modelManager=modelManager, dataLoader=unconfidentDataloader, numCentroids=numCentroids)
+		confidentEmbeddings, confidentCentroids = self._getEmbeddingsAndCentroids(modelManager=modelManager, dataLoader=confidentDataloader, numCentroids=numCentroids)
+
+		# Add embeddings to dataloaders
+		unconfidentDataloader.dataset = unconfidentDataloader.dataset.add_column("embeddings", unconfidentEmbeddings)
+		confidentDataloader.dataset = confidentDataloader.dataset.add_column("embeddings", confidentEmbeddings)
+
+		# Calculate C-score for each unconfident datapoint
+		unconfidentWithCScores = self._calculateCScores(
+			modelManager=modelManager,
+			unconfidentDataLoader=unconfidentDataloader,
+			unconfidentCentroids=unconfidentCentroids,
+			confidentCentroids=confidentCentroids
+		)
+
+		# Get top datapoints with highest C-scores
+		finalistDataLoader = self._getTopCScores(
+			dataLoader=unconfidentWithCScores,
+			numFinalists=numFinalists
+		)
+		return finalistDataLoader
+
+
+	def _getEmbeddingsAndCentroids(self, modelManager: HSSFLDON_ModelManager, dataLoader: DataLoader, numCentroids: int) -> tuple[torch.Tensor, torch.Tensor]:
+		"""
+		Gets the embeddings and centroids for a given dataloader using KMeans clustering on model embeddings.
+		"""
+		self.logger.debug(f"Calculating {numCentroids} centroids for dataloader with {len(dataLoader)} datapoints!")
+
+		# Get embeddings for each datapoint using model
+		embeddings, _ = modelManager.predict(
+			dataLoader=dataLoader,
+			outputType=HSSFLDON_PredictionOutputType.EMBEDDING_PREDICTION
+		)
+
+		# Perform KMeans clustering on embeddings to get centroids
+		kmeans = KMeans(n_clusters=numCentroids, random_state=0)
+		kmeans.fit(embeddings.cpu().numpy())
+		centroids = kmeans.cluster_centers_
+
+		# Return centroids
+		return embeddings, torch.from_numpy(centroids)
+	
+	def _calculateCScores(self, modelManager: HSSFLDON_ModelManager, unconfidentDataLoader: DataLoader, unconfidentCentroids: torch.Tensor, confidentCentroids: torch.Tensor) -> DataLoader:
+		"""
+		Calculate C-score as described in the paper.
+		"""
+		self.logger.debug(f"Calculating C-scores for unconfident candidates!")
+
+		# Batch embeddings and send to device
+		allEmbeddings = []
+		with torch.no_grad():
+			for batch in unconfidentDataLoader:
+				embeddings = batch["embeddings"].to(modelManager.device)
+				allEmbeddings.append(embeddings)
+
+		# Concat embeddings into tensor
+		allEmbeddings = torch.cat(allEmbeddings, dim=0)
+
+		# Calculate cosine similarities to each centroid
+		unconfidentCentroidSimilarities = F.cosine_similarity(
+			x1=allEmbeddings.unsqueeze(1), 
+			x2=unconfidentCentroids.unsqueeze(0),
+			dim=2
+		)
+		confidentCentroidSimilarities = F.cosine_similarity(
+			x1=allEmbeddings.unsqueeze(1), 
+			x2=confidentCentroids.unsqueeze(0),
+			dim=2
+		)
+
+		# Calculate average similarities for each datapoint
+		averageUnconfidentSimilarity = unconfidentCentroidSimilarities.mean(dim=1)
+		averageConfidentSimilarity = confidentCentroidSimilarities.mean(dim=1)
+
+		# Compute final C-scores as avg(unconfident) - avg(confident) for each datapoint
+		cScores = averageUnconfidentSimilarity - averageConfidentSimilarity
+		return unconfidentDataLoader.dataset.add_column("c_scores", cScores.cpu())
+	
+	def _getTopCScores(self, dataLoader: DataLoader, numFinalists: int) -> DataLoader:
+		"""
+		Get the top N datapoints with the highest C-scores from the dataloader.
+		"""
+		self.logger.debug(f"Getting top {numFinalists} datapoints with highest C-scores!")
+
+		# Sort dataloader by C-score and get top N
+		sortedData = sorted(dataLoader.dataset, key=lambda x: x["c_scores"], reverse=True)
+		topData = sortedData[:numFinalists]
+		return DataLoader(topData)
