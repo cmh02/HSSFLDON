d4c74b34aef912a2589e1f7e555a4d59f75ca1fe
cmh02 <cmh0201@auburn.edu>
2026-04-30 15:16:46 -0500

    Fixing clamp to be post-tensor-conversion

--- src/hssfldon/common/hssfldon_model.py
+++ src/hssfldon/common/hssfldon_model.py
@@ -68,8 +68,8 @@ class HSSFLDON_ModelManager:
 		self.classPositionWeights = [
 			float(os.getenv(f"HSSFLDON_CLASS_POSWEIGHT_{i}", 1.0)) for i in range(self.modelNClasses)
 		]
-		self.classPositionWeights = torch.clamp(self.classPositionWeights, min=1.0, max=5.0)
 		self.classPositionWeights = torch.tensor(self.classPositionWeights, device=self.device)
+		self.classPositionWeights = torch.clamp(self.classPositionWeights, min=1.0, max=5.0)
 
 		# Create static paths for model components
 		self.modelPath_base: str = os.path.join(self.modelDirectory, f"model_base")
