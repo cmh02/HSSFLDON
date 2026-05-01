fe29266e9adbad4752956c3812690e02c6ea8a77
cmh02 <cmh0201@auburn.edu>
2026-05-01 13:07:59 -0500

    Server now enforces temperature scaling with active learning

--- src/hssfldon/server/server_app.py
+++ src/hssfldon/server/server_app.py
@@ -348,7 +348,8 @@ class HSSFLDON_ServerApplication:
 				labels = self.unlabeledDataset["labels"]
 			)
 			output, _ = modelManager.predict(
-				dataLoader=allUnlabeledCandidatesDataloader
+				dataLoader=allUnlabeledCandidatesDataloader,
+				doTemperatureScaling=True
 			)
 			self.logger.info(f"Calculated probabilities for unlabeled dataset for round {learningRound}/{self.learningRounds}!")
 			activeDataSet = allUnlabeledCandidatesDataloader.dataset
